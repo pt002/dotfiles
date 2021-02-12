7a71ba45caa8d384b1f54afdc79885f0a7fdf8fb
PT <ptongsak@gmail.com>
2021-02-12 11:54:39 -0800

    update apps, bootstrap, and aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -120,6 +120,12 @@ for b_casks in ${casks[@]}; do
   cask_install ${b_casks}
 done
 
+bot "installing speedtest"
+brew tap teamookla/speedtest
+for b_speed in ${speed[@]}; do
+  cask_install ${b_speed} --force
+done
+
 bot "installing brew fonts"
 brew tap homebrew/cask-fonts
 for b_fonts in ${fonts[@]}; do
