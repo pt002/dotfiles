16d5a6419e4460b580761b80ae0e7e2b219b4f97
Philip Tongsak <pt002@github.com>
2022-10-25 13:45:15 -0700

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -70,15 +70,15 @@ bot "install homebrew"
 brew_check=$(which brew) 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
   action "installing homebrew. press return to continue."
-#  if [ "$(uname -p)" = "arm" ]; then
-#    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-#    eval "$(/opt/homebrew/bin/brew shellenv)"
-#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &>> ${log_dir}/${logfile}
-#    BREW_PREFIX=$(brew --prefix)
-#  else
+  if [ "$(uname -p)" = "arm" ]; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)" &>> ${log_dir}/${logfile}
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &>> ${log_dir}/${logfile}
     BREW_PREFIX=$(brew --prefix)
-#  fi
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" &>> ${log_dir}/${logfile}
+    BREW_PREFIX=$(brew --prefix)
+  fi
 else
   print "homebrew already installed..."
   BREW_PREFIX=$(brew --prefix)
