cd9d4506b3750de09024ef7905523d9ccaefb03d
Philip Tongsak <pt002@github.com>
2026-01-21 15:42:40 -0800

    update views

--- macos-preferences.sh
+++ macos-preferences.sh
@@ -244,25 +244,26 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop     -bool false; ok
 running "hide desktop widgets"
 defaults write com.apple.WindowManager StandardHideWidgets -bool true; ok
 
-# # Set icon view settings on desktop and in icon views
-# running "set icon view settings and options"
-# for view in 'Desktop' 'FK_Standard' 'Standard'; do
-# 
-#   # Snap-to-grid for icons
-#   #/usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:arrangeBy string grid" $HOME/Library/Preferences/com.apple.finder.plist
-# 
-#   # Grid spacing for icons
-#   /usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:gridSpacing 1" $HOME/Library/Preferences/com.apple.finder.plist
-# 
-#   # Icon size
-#   /usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:iconSize 16" $HOME/Library/Preferences/com.apple.finder.plist
-# 
-#   # Text size
-#   /usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:textSize 10" $HOME/Library/Preferences/com.apple.finder.plist
-# 
-# done
-# ok
-# 
+# Set icon view settings on desktop and in icon views
+running "set icon view settings and options"
+#for view in 'Desktop' 'FK_Standard' 'Standard'; do
+for view in 'Desktop'; do
+
+  # Snap-to-grid for icons
+  #/usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:arrangeBy string grid" $HOME/Library/Preferences/com.apple.finder.plist
+
+  # Grid spacing for icons
+  /usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:gridSpacing 1" $HOME/Library/Preferences/com.apple.finder.plist
+
+  # Icon size
+  /usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:iconSize 16" $HOME/Library/Preferences/com.apple.finder.plist
+
+  # Text size
+  /usr/libexec/PlistBuddy -c "Set :${view}ViewSettings:IconViewSettings:textSize 10" $HOME/Library/Preferences/com.apple.finder.plist
+
+done
+ok
+ 
 # # Set list view settings
 # running "set list view settings and options"
 # for view in 'FK_Standard' 'Standard'; do
@@ -275,28 +276,28 @@ defaults write com.apple.WindowManager StandardHideWidgets -bool true; ok
 # 
 # done
 # ok
-# 
-# # View Options
-# # ColumnShowIcons    : Show preview column
-# # ShowPreview        : Show icons
-# # ShowIconThumbnails : Show icon preview
-# # ArrangeBy          : Sort by
-# #   dnam : Name
-# #   kipl : Kind
-# #   ludt : Date Last Opened
-# #   pAdd : Date Added
-# #   modd : Date Modified
-# #   ascd : Date Created
-# #   logs : Size
-# #   labl : Tags
-# running "set column view settings and options"
-# /usr/libexec/PlistBuddy \
-#     -c "Set :StandardViewOptions:ColumnViewOptions:ColumnShowIcons true" \
-#     -c "Set :StandardViewOptions:ColumnViewOptions:FontSize        10"    \
-#     -c "Set :StandardViewOptions:ColumnViewOptions:ShowPreview     true"  \
-#     -c "Set :StandardViewOptions:ColumnViewOptions:ArrangeBy       dnam"  \
-#     $HOME/Library/Preferences/com.apple.finder.plist
-# ok
+
+# View Options
+# ColumnShowIcons    : Show preview column
+# ShowPreview        : Show icons
+# ShowIconThumbnails : Show icon preview
+# ArrangeBy          : Sort by
+#   dnam : Name
+#   kipl : Kind
+#   ludt : Date Last Opened
+#   pAdd : Date Added
+#   modd : Date Modified
+#   ascd : Date Created
+#   logs : Size
+#   labl : Tags
+running "set column view settings and options"
+/usr/libexec/PlistBuddy \
+    -c "Set :StandardViewOptions:ColumnViewOptions:ColumnShowIcons true" \
+    -c "Set :StandardViewOptions:ColumnViewOptions:FontSize        10"    \
+    -c "Set :StandardViewOptions:ColumnViewOptions:ShowPreview     true"  \
+    -c "Set :StandardViewOptions:ColumnViewOptions:ArrangeBy       dnam"  \
+    $HOME/Library/Preferences/com.apple.finder.plist
+ok
 
 # Preferred view style
 # Icon View   : `icnv`
